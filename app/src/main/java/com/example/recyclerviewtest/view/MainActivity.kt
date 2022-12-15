fcc5e96f573a0619959fc4ad0703a669bd62bbda
L <leonardo.alvirubro@hotmail.com>
2022-12-15 03:17:03 -0300

    style: list item XML created

--- app/src/main/java/com/example/recyclerviewtest/view/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewtest/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.recyclerviewtest
+package com.example.recyclerviewtest.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recyclerviewtest.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
